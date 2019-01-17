90582c42cf1d1e2abc0d5b029c612d36e6cb7565
William Chong <williamchong007@msn.com>
2019-01-17 13:51:24 +0800

    ♻️ Clean up test related code/settings

--- test/stub/runner.js
+++ test/stub/runner.js
@@ -8,14 +8,12 @@ function setStub() {
   execSync('cp ./test/stub/util/* ./src/util/');
   execSync('cp ./config/accounts.js ./config/accounts.js.bak || true');
   execSync('cp ./test/stub/config/accounts.js ./config/accounts.js || true');
-  execSync('sed -i.bak "s/0xB97Df12b24C119A052EE0D4Ba97bAc59Da86AB4B/0x2fDF85d31b023c471a7F54cF2E67bA5767ADaECa/" ./src/constant/contract/likecoin.js');
 }
 
 function unsetStub() {
   console.log('Unsetting Stub');
   execSync('mv ./src/util/firebase.js.bak ./src/util/firebase.js');
   execSync('mv ./src/util/ses.js.bak ./src/util/ses.js');
-  execSync('mv ./src/constant/contract/likecoin.js.bak ./src/constant/contract/likecoin.js');
   execSync('mv ./config/accounts.js.bak ./config/accounts.js');
 }
 
