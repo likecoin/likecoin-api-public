595509f611f370d3eb926f224de1bfbc01a852fb
Ng Wing Tat, David <i@ngwingt.at>
2019-01-24 14:12:38 +0800

    ✨ Add joining trial event API

--- src/routes/civic/index.js
+++ src/routes/civic/index.js
@@ -9,6 +9,7 @@ import {
 import publisher from '../../util/gcloudPub';
 import {
   userCollection as dbRef,
+  subscriptionUserCollection as subscriptionDbRef,
   configCollection as configRef,
 } from '../../util/firebase';
 
@@ -157,6 +158,83 @@ router.get('/trial/events/:id', async (req, res, next) => {
     }
   } catch (err) {
     console.error(err);
+    next(err);
+  }
+});
+
+router.post('/trial/events/:eventId/join', jwtAuth('write'), async (req, res, next) => {
+  try {
+    const { eventId } = req.params;
+    const userId = req.user.user;
+
+    const userDoc = await dbRef.doc(userId).get();
+    if (!userDoc.exists) throw new Error('USER_NOT_EXIST');
+
+    const subscriptionDoc = await subscriptionDbRef.doc(userId).get();
+    if (subscriptionDoc.exists) {
+      // User has past/active subscription
+      res.sendStatus(409);
+      return;
+    }
+
+    const trialEventDoc = await configRef
+      .doc('civicLiker')
+      .collection('trialEvents')
+      .doc(eventId)
+      .get();
+
+    if (!trialEventDoc.exists) {
+      res.sendStatus(404);
+      return;
+    }
+
+    const { start, end } = trialEventDoc.data();
+    const now = Date.now();
+    if (now < start) {
+      res.sendStatus(404);
+      return;
+    }
+    if (now > end) {
+      res.sendStatus(410);
+      return;
+    }
+
+    const trialEnd = new Date(now);
+    trialEnd.setMonth(trialEnd.getMonth() + 1);
+    const createObj = {
+      civicLikerStatus: 'subscribed',
+      price: 5,
+      since: now,
+      currentType: 'trial',
+      currentPeriodStart: now,
+      currentPeriodEnd: trialEnd.getTime(),
+    };
+    await subscriptionDbRef.doc(userId).create(createObj);
+
+    res.json({
+      start: createObj.currentPeriodStart,
+      end: createObj.currentPeriodEnd,
+    });
+
+    const {
+      email,
+      displayName,
+      wallet,
+      referrer,
+      locale,
+      timestamp: registerTime,
+    } = userDoc.data();
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'eventCivicLikerTrial',
+      user: userId,
+      email,
+      displayName,
+      wallet,
+      referrer,
+      locale,
+      registerTime,
+    });
+  } catch (err) {
     next(err);
   }
 });
