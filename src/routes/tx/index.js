23201a809798d69fe3635f857fe2c68289d211ae
William Chong <williamchong007@msn.com>
2019-06-18 19:09:54 +0800

    ✨ Add query tx history by username

--- src/routes/tx/index.js
+++ src/routes/tx/index.js
@@ -20,20 +20,20 @@ const router = Router();
 
 function filterMultipleTxData(data, filter = {}) {
   const { to, toIds, value } = data;
-  const { to: { addresses = [] } = {} } = filter;
+  const { to: { addresses, id } = {} } = filter;
   const result = {};
   to.forEach((addr, index) => {
-    if (!addresses || addresses.includes(addr)) {
-      result[addr] = result[addr]
-        || {
-          id: toIds[index],
-          value: new BigNumber(0),
-        };
-      if (result[addr].id !== toIds[index]) {
-        throw new Error(`Filter ID ${toIds[index]} found, expected: ${result[addr].id}`);
-      }
-      result[addr].value = result[addr].value.plus(new BigNumber(value[index]));
+    if (addresses && !addresses.includes(addr)) return;
+    if (id && toIds[index] !== id) return;
+    result[addr] = result[addr]
+      || {
+        id: toIds[index],
+        value: new BigNumber(0),
+      };
+    if (result[addr].id !== toIds[index]) {
+      throw new Error(`Filter ID ${toIds[index]} found, expected: ${result[addr].id}`);
     }
+    result[addr].value = result[addr].value.plus(new BigNumber(value[index]));
   });
   // Flatten the result to arrays.
   const tos = Object.keys(result);
@@ -64,6 +64,57 @@ router.get('/id/:id', async (req, res, next) => {
       return;
     }
     res.sendStatus(404);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/history/user/:id', jwtAuth('read'), async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (req.user.user !== id) {
+      res.status(401).send('LOGIN_NEEDED');
+      return;
+    }
+
+    let { ts, count } = req.query;
+    ts = Number(ts);
+    if (!ts || Number.isNaN(ts)) ts = Date.now();
+    count = Number(count);
+    if (!count || Number.isNaN(count) || count > TRANSACTION_QUERY_LIMIT) {
+      count = TRANSACTION_QUERY_LIMIT;
+    }
+    const queryTo = txLogRef
+      .where('toId', '==', id)
+      .orderBy('ts', 'desc')
+      .startAt(ts)
+      .limit(count)
+      .get();
+    const queryToArray = txLogRef
+      .where('toIds', 'array-contains', id)
+      .orderBy('ts', 'desc')
+      .startAt(ts)
+      .limit(count)
+      .get();
+    const queryFrom = txLogRef
+      .where('fromId', '==', id)
+      .orderBy('ts', 'desc')
+      .startAt(ts)
+      .limit(count)
+      .get();
+    const [dataTo, dataToArray, dataFrom] = await Promise.all([queryTo, queryToArray, queryFrom]);
+    let results = dataTo.docs.concat(dataToArray.docs).concat(dataFrom.docs);
+    results = results.map((d) => {
+      const data = d.data().toIds
+        ? filterMultipleTxData(d.data(), {
+          to: { id },
+        })
+        : d.data();
+      return { id: d.id, ...filterTxData(data) };
+    });
+    results.sort((a, b) => (b.ts - a.ts));
+    results.splice(count);
+    res.json(results);
   } catch (err) {
     next(err);
   }
