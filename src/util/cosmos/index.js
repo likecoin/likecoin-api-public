875be9fef8524a8d4e0c095ee664018baad6e284
William Chong <williamchong007@msn.com>
2019-08-01 17:52:14 +0800

    🎨 Modify cosmos util interface

--- src/util/cosmos/index.js
+++ src/util/cosmos/index.js
@@ -1,25 +1,30 @@
 import axios from 'axios';
 import {
-  COSMOS_LCD_ENDPOINT,
+  COSMOS_LCD_ENDPOINT as cosmosLCDEndpoint,
   COSMOS_DENOM,
 } from '../../../config/config';
 
-const api = axios.create({ baseURL: `http://${COSMOS_LCD_ENDPOINT}` });
+const api = axios.create({ baseURL: `http://${cosmosLCDEndpoint}` });
 
-function likeToNanolike(value) {
+function LIKEToNanolike(value) {
   return `${Number.parseInt(value, 10).toString()}000000000`;
 }
 
-export function likeToAmount(value) {
-  return { denom: COSMOS_DENOM, amount: likeToNanolike(value) };
+export function LIKEToAmount(value) {
+  return { denom: COSMOS_DENOM, amount: LIKEToNanolike(value) };
+}
+export function amountToLIKE(likecoin) {
+  if (likecoin.denom === 'nanolike') {
+    return (Number.parseFloat(likecoin.amount) / 1e9);
+  }
+  console.error(`${likecoin.denom} is not supported denom`);
+  return -1;
 }
 
-function amountToLike(likecoin) {
-  return (Number.parseFloat(likecoin.amount) / 1e9);
-}
-
-export async function getCosmosAccountInfo(address) {
+export async function getCosmosAccountLIKE(address) {
   const { data } = await api.get(`/auth/accounts/${address}`);
   const likecoin = data.coins.find(c => c.denom === COSMOS_DENOM);
-  return likecoin ? amountToLike(likecoin) : 0;
+  return likecoin ? amountToLIKE(likecoin) : 0;
 }
+
+export const COSMOS_LCD_ENDPOINT = cosmosLCDEndpoint;
