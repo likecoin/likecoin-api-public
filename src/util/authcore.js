f4f42150ae25b60297664e2eb2e3d87489c0a668
William Chong <williamchong007@msn.com>
2019-11-22 21:39:10 +0800

    👽 Use new authcore vault API

--- src/util/authcore.js
+++ src/util/authcore.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import {
   AUTHCORE_API_ENDPOINT,
+  AUTHCORE_SECRETD_STATIC_KEY,
 } from '../../config/config';
 
-const hdkey = require('hdkey');
-const bech32 = require('bech32');
+const { AuthcoreVaultClient, AuthcoreCosmosProvider } = require('secretd-js');
 
-const api = axios.create({ baseURL: AUTHCORE_API_ENDPOINT });
+const api = axios.create({ baseURL: `${AUTHCORE_API_ENDPOINT}/api` });
 
 export async function getAuthCoreUser(accessToken) {
   const { data } = await api.get('/auth/users/current', {
@@ -49,47 +49,28 @@ export async function registerAuthCoreUser(payload, accessToken) {
 }
 
 export async function getAuthCoreCosmosWallet(userId, accessToken) {
-  const { data: listData } = await api.post('/keyvault/operation', {
-    list_hd_child_public_keys: {
-      path: "m/44'/118'/0'/0/0",
-      as_user: userId,
-    },
-  }, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+  const client = new AuthcoreVaultClient({
+    apiBaseURL: AUTHCORE_API_ENDPOINT,
+    accessToken,
+    staticKey: AUTHCORE_SECRETD_STATIC_KEY,
   });
-  if (!listData
-    || !listData.hd_child_public_keys
-    || listData.hd_child_public_keys.length < 1) {
+  const uid = await client.authcoreLookupOrCreateUser(userId);
+  const cosmosProvider = new AuthcoreCosmosProvider({
+    client,
+    oid: `user/${uid}/hdwallet_default`,
+  });
+  const addresses = await cosmosProvider.getAddresses();
+  if (!addresses || addresses.length < 1) {
     return '';
   }
-  const [key] = listData.hd_child_public_keys;
-  const xpub = hdkey.fromExtendedKey(key.extended_public_key);
-  const address = bech32.encode('cosmos', bech32.toWords(xpub.identifier));
+  const [address] = addresses;
   return address;
 }
 
 export async function createAuthCoreCosmosWallet(userId, accessToken) {
-  await api.post('/keyvault/operation',
-    {
-      create_secret: {
-        type: 'HD_KEY',
-        size: 24,
-        as_user: userId,
-      },
-    }, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-  const address = await getAuthCoreCosmosWallet(userId, accessToken);
-  return address;
+  return getAuthCoreCosmosWallet(userId, accessToken);
 }
 
 export async function createAuthCoreCosmosWalletIfNotExist(userId, accessToken) {
-  let address = await getAuthCoreCosmosWallet(userId, accessToken);
-  if (address) return address;
-  address = await createAuthCoreCosmosWallet(userId, accessToken);
-  return address;
+  return getAuthCoreCosmosWallet(userId, accessToken);
 }
