7b66e6907ec5c962490a84b59992794dbb295c6f
Chung Wu <chung@like.co>
2019-12-05 14:35:09 +0800

    🔧 Update reserved wallet addresses in total supply api

--- src/routes/misc/totalSupply.js
+++ src/routes/misc/totalSupply.js
@@ -6,7 +6,13 @@ import { IS_TESTNET } from '../../constant';
 const router = Router();
 
 /* wallets used for migration/burn/vault/etc */
-const reservedWallets = IS_TESTNET ? ['cosmos1ca0zlqxjqv5gek5qxm602umtkmu88564hpyws4'] : [];
+const reservedWallets = IS_TESTNET ? [
+  'cosmos1ca0zlqxjqv5gek5qxm602umtkmu88564hpyws4',
+] : [
+  'cosmos1xxr3yfvr0zc6dqdy7jttjh6nvupx9j08d3v538', // ecosystem development pool 1
+  'cosmos1rr8km790vqdgl6h97hz7ghlatad87jnyrh2qka', // ecosystem development pool 2
+  'cosmos1sltfqp94nmmzkgvwrfqwl5f34u0rfdxq2e5a6c', // team pool
+];
 
 router.get('/totalsupply', async (req, res) => {
   const ONE_DAY_IN_S = 3600;
