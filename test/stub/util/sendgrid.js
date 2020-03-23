4c750029ccea9f60f1766bec0e0f23615481b053
William Chong <williamchong007@msn.com>
2020-03-23 18:44:52 +0800

    ✅ Add sendgrid stub

---
+++ test/stub/util/sendgrid.js
@@ -0,0 +1,16 @@
+console.log('Using stub (sendgrid.js)'); /* eslint no-console: "off" */
+
+/* istanbul ignore next */
+export async function sendVerificationEmail() {
+  return Promise.resolve();
+}
+
+/* istanbul ignore next */
+export async function sendVerificationWithCouponEmail() {
+  return Promise.resolve();
+}
+
+/* istanbul ignore next */
+export async function sendInvitationEmail() {
+  return Promise.resolve();
+}
