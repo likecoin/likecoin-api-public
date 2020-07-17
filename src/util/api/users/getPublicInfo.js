af6ef14cc6df7c4bb4284b3f8cd16282592e3d35
William Chong <williamchong007@msn.com>
2020-07-17 21:31:35 +0800

    ♻️ Refactor user subscription info fetch

--- src/util/api/users/getPublicInfo.js
+++ src/util/api/users/getPublicInfo.js
@@ -5,29 +5,28 @@ import {
 } from '../../../constant';
 import {
   userCollection as dbRef,
-  subscriptionUserCollection as subscriptionDbRef,
 } from '../../firebase';
 
 export async function getUserWithCivicLikerProperties(id) {
-  const [userDoc, subscriptionDoc] = await Promise.all([
+  const [userDoc] = await Promise.all([
     dbRef.doc(id).get(),
-    subscriptionDbRef.doc(id).get(),
   ]);
   if (!userDoc.exists) return null;
 
   const payload = userDoc.data();
+  const { avatar, civicLiker } = payload;
   payload.user = id;
-  if (!payload.avatar) {
+  if (!avatar) {
     payload.avatar = AVATAR_DEFAULT_PATH;
   }
 
-  if (subscriptionDoc.exists) {
+  if (civicLiker) {
     const {
       currentPeriodStart: start,
       currentPeriodEnd: end,
       since,
       currentType,
-    } = subscriptionDoc.data();
+    } = civicLiker;
     const now = Date.now();
     const renewalLast = end + SUBSCRIPTION_GRACE_PERIOD;
     if (start <= now && now <= renewalLast) {
