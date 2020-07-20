f53b13de840747323b613f721a81f72d6cc848f6
William Chong <williamchong007@msn.com>
2020-07-20 22:49:52 +0800

    ♻️ Refactor civic trial handler to external webhook

--- src/routes/civic/index.js
+++ src/routes/civic/index.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import {
-  PUBSUB_TOPIC_MISC,
-} from '../../constant';
+import axios from 'axios';
+
 import { jwtAuth } from '../../middleware/jwt';
-import publisher from '../../util/gcloudPub';
-import {
-  db,
-  userCollection as dbRef,
-  subscriptionUserCollection as subscriptionDbRef,
-  configCollection as configRef,
-} from '../../util/firebase';
+import { configCollection as configRef } from '../../util/firebase';
+import { CIVIC_LIKER_TRIAL_WEBHOOK } from '../../../config/config';
 
 const router = Router();
 
@@ -52,93 +46,25 @@ router.get('/trial/events/:id', async (req, res, next) => {
 
 router.post('/trial/events/:eventId/join', jwtAuth('write:civic_liker'), async (req, res, next) => {
   try {
-    const { eventId } = req.params;
-    const userId = req.user.user;
-
-    const userDoc = await dbRef.doc(userId).get();
-    if (!userDoc.exists) throw new Error('USER_NOT_EXIST');
-
-    const subscriptionDoc = await subscriptionDbRef.doc(userId).get();
-    if (subscriptionDoc.exists) {
-      // User has past/active subscription
-      res.sendStatus(409);
+    if (!CIVIC_LIKER_TRIAL_WEBHOOK) {
+      res.status(500).send('TRIAL_ENDPOINT_NOT_CONFIGURED');
       return;
     }
-
-    const subscription = await db.runTransaction(async (t) => {
-      const trialEventRef = configRef
-        .doc('civicLiker')
-        .collection('trialEvents')
-        .doc(eventId);
-      const trialEventDoc = await trialEventRef.get();
-
-      if (!trialEventDoc.exists) throw new Error('TRIAL_EVENT_NOT_FOUND');
-
-      const {
-        start,
-        end,
-        regCount,
-        regQuota,
-      } = trialEventDoc.data();
-      const now = Date.now();
-      if (now < start) throw new Error('TRIAL_EVENT_NOT_STARTED');
-      if (now > end) throw new Error('TRIAL_EVENT_EXPIRED');
-      if (regCount >= regQuota) throw new Error('TRIAL_EVENT_FULL');
-
-      const trialEnd = new Date(now);
-      trialEnd.setMonth(trialEnd.getMonth() + 1);
-      const createObj = {
-        eventId,
-        since: now,
-        currentType: 'trial',
-        currentPeriodStart: now,
-        currentPeriodEnd: trialEnd.getTime(),
-      };
-      await t.update(trialEventRef, { regCount: regCount + 1 });
-      await t.create(subscriptionDbRef.doc(userId), createObj);
-      return createObj;
-    });
-
-    res.json({
-      start: subscription.currentPeriodStart,
-      end: subscription.currentPeriodEnd,
-    });
-
-    const {
-      email,
-      displayName,
-      wallet,
-      referrer,
-      locale,
-      timestamp: registerTime,
-      mediaChannels,
-    } = userDoc.data();
-    publisher.publish(PUBSUB_TOPIC_MISC, req, {
-      logType: 'eventCivicLikerTrial',
-      user: userId,
-      email,
-      displayName,
-      wallet,
-      referrer,
-      locale,
-      registerTime,
-      mediaChannels,
-    });
+    const { eventId } = req.params;
+    const payload = req.body || {};
+    const response = await axios.post(
+      CIVIC_LIKER_TRIAL_WEBHOOK,
+      { ...payload, eventId },
+      {
+        headers: req.headers,
+      },
+    );
+    res.status(response.status).header(response.headers).send(response.data);
   } catch (err) {
-    if (err && err.message) {
-      switch (err.message) {
-        case 'TRIAL_EVENT_NOT_FOUND':
-        case 'TRIAL_EVENT_NOT_STARTED':
-          res.sendStatus(404);
-          return;
-
-        case 'TRIAL_EVENT_FULL':
-        case 'TRIAL_EVENT_EXPIRED':
-          res.sendStatus(410);
-          return;
-
-        default:
-      }
+    if (err.reponse) {
+      const { response } = err;
+      res.status(response.status).header(response.headers).send(response.data);
+      return;
     }
     next(err);
   }
