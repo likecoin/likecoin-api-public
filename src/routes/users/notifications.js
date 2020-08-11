6b05cd68090093bcf52bfd7e34dbaf8a520bcb41
Ng Wing Tat, David <i@ngwingt.at>
2020-08-11 14:58:22 +0800

    ✨ Update notification API (#185)
    * ✨ Update notification API
    
    * 🥅 Cap operations up to 500 per batch
    
    * ✏️ Correct invalid `before` response message
    
    * 🎨 Split limit input

--- src/routes/users/notifications.js
+++ src/routes/users/notifications.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { userCollection as dbRef } from '../../util/firebase';
+import { db, userCollection as dbRef } from '../../util/firebase';
 import { filterNotification } from '../../util/ValidationHelper';
 import { jwtAuth } from '../../middleware/jwt';
 
@@ -8,18 +8,84 @@ const router = Router();
 router.get('/notifications', jwtAuth('read:notifications'), async (req, res, next) => {
   try {
     const { user } = req.user;
-    const { limit = 64 } = req.query;
-    const query = await dbRef
+    let queryRef = dbRef
       .doc(user)
       .collection('notifications')
-      .orderBy('ts', 'desc')
-      .limit(limit)
-      .get();
+      .orderBy('ts', 'desc');
+
+    let { after, before } = req.query;
+    if (after) {
+      try {
+        after = Number(after);
+        queryRef = queryRef.endBefore(after);
+      } catch (err) {
+        // no-op
+      }
+    }
+    if (before) {
+      try {
+        before = Number(before);
+        queryRef = queryRef.startAfter(before);
+      } catch (err) {
+        // no-op
+      }
+    }
+    let limit;
+    const { limit: limitInput } = req.query;
+    if (limitInput) {
+      try {
+        limit = Number(limitInput);
+      } catch (err) {
+        // no-op
+      }
+    }
+    if (!limit) {
+      limit = 64;
+    }
+    const query = await queryRef.limit(limit).get();
     const list = [];
     query.docs.forEach((d) => {
       list.push(filterNotification({ id: d.id, ...d.data() }));
     });
     res.json({ list });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/notifications/read', jwtAuth('write:notifications'), async (req, res, next) => {
+  try {
+    const { user } = req.user;
+    let { before } = req.query;
+    if (before) {
+      try {
+        before = Number(before);
+      } catch (err) {
+        res.status(400).send('INVALID_BEFORE');
+        return;
+      }
+    }
+    const query = await dbRef
+      .doc(user)
+      .collection('notifications')
+      .where('isRead', '==', false)
+      .orderBy('ts', 'desc')
+      .startAfter(before)
+      .get();
+    if (query.docs.length) {
+      const batchCommits = [];
+      const maxOpsPerBatch = 500;
+      for (let i = 0; i < query.docs.length; i += maxOpsPerBatch) {
+        const batch = db.batch();
+        const docs = query.docs.slice(i, i + maxOpsPerBatch);
+        docs.forEach((doc) => {
+          batch.update(doc.ref, { isRead: true });
+        });
+        batchCommits.push(batch.commit());
+      }
+      await Promise.all(batchCommits);
+    }
+    res.sendStatus(200);
   } catch (err) {
     next(err);
   }
