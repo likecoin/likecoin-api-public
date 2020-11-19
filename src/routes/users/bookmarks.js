33038a6d4953994229749ce6e4d9768b5a771baf
William Chong <me@williamchong.cloud>
2020-11-19 20:05:37 +0800

    ✨ Remove known tracking qs before saving bookmark

--- src/routes/users/bookmarks.js
+++ src/routes/users/bookmarks.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { userCollection as dbRef } from '../../util/firebase';
 import { filterBookmarks } from '../../util/ValidationHelper';
 import { jwtAuth } from '../../middleware/jwt';
-import { addUrlToMetadataCrawler } from '../../util/api/users/bookmarks';
+import { addUrlToMetadataCrawler, removeQueryStringFromBookmarkUrl } from '../../util/api/users/bookmarks';
 import { PUBSUB_TOPIC_MISC, API_DEFAULT_SIZE_LIMIT } from '../../constant';
 import publisher from '../../util/gcloudPub';
 
@@ -46,20 +46,24 @@ router.get('/bookmarks/:id?', jwtAuth('read:bookmarks'),
   async (req, res, next) => {
     try {
       const bookmarkID = req.params.id;
-      const url = req.body.url || req.query.url;
-      if (url && bookmarkID) {
+      const inputUrl = req.body.url || req.query.url;
+      if (inputUrl && bookmarkID) {
         res.status(400).send('URL_AND_ID_COEXIST');
         return;
       }
-      if (!url && !bookmarkID) {
+      if (!inputUrl && !bookmarkID) {
         next();
         return;
       }
-      try {
-        urlParse(url);
-      } catch (err) {
-        res.status(400).send('INVALID_URL');
-        return;
+      let url = inputUrl;
+      if (inputUrl) {
+        try {
+          urlParse(inputUrl);
+          url = removeQueryStringFromBookmarkUrl(inputUrl);
+        } catch (err) {
+          res.status(400).send('INVALID_URL');
+          return;
+        }
       }
       const { user } = req.user;
       const doc = await queryBookmark(user, { bookmarkID, url });
@@ -128,17 +132,18 @@ router.get('/bookmarks/:id?', jwtAuth('read:bookmarks'),
 router.post('/bookmarks', jwtAuth('write:bookmarks'), async (req, res, next) => {
   try {
     const { user } = req.user;
-    const url = req.body.url || req.query.url;
-    if (!url) {
+    const inputUrl = req.body.url || req.query.url;
+    if (!inputUrl) {
       res.status(400).send('MISSING_URL');
       return;
     }
     try {
-      urlParse(url);
+      urlParse(inputUrl);
     } catch (err) {
       res.status(400).send('INVALID_URL');
       return;
     }
+    const url = removeQueryStringFromBookmarkUrl(inputUrl);
     const query = await dbRef
       .doc(user)
       .collection('bookmarks')
@@ -158,12 +163,14 @@ router.post('/bookmarks', jwtAuth('write:bookmarks'), async (req, res, next) => 
         isArchived: false,
         ts: Date.now(),
         url,
+        originalUrl: inputUrl,
       });
 
     publisher.publish(PUBSUB_TOPIC_MISC, req, {
       logType: 'userBookmarkAdd',
       user,
       url,
+      originalUrl: inputUrl,
     });
     res.json({
       id: bookmarkID,
--- src/util/api/users/bookmarks.js
+++ src/util/api/users/bookmarks.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { QUERY_STRING_TO_REMOVE } from '../../../constant';
 import {
   LIKECO_INTERNAL_API_ENDPOINT,
   LIKECO_INTERNAL_API_KEY,
 } from '../../../../config/config';
 
 const querystring = require('querystring');
+const URL = require('url-parse');
 
 export function addUrlToMetadataCrawler(url) {
   return axios.post(
@@ -16,6 +18,20 @@ export function addUrlToMetadataCrawler(url) {
       },
     },
   );
+}
+
+export function removeQueryStringFromBookmarkUrl(inputUrl) {
+  try {
+    const url = new URL(inputUrl, true);
+    QUERY_STRING_TO_REMOVE.forEach((qs) => {
+      delete url.query[qs];
+    });
+    url.set('query', url.query);
+
+    return url.toString();
+  } catch (err) {
+    return '';
+  }
 }
 
 export default addUrlToMetadataCrawler;
