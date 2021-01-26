d1774fb2b904d7a31a1425c0c1ab218e0b602b7f
William Chong <me@williamchong.cloud>
2021-01-26 18:50:16 +0800

    ✨ Add metadata tx update condition guard

--- src/constant/index.js
+++ src/constant/index.js
@@ -131,4 +131,5 @@ export const LIKE_DEFAULT_PRICE = 0.0082625;
 
 export const KICKBOX_DISPOSIBLE_API = 'https://open.kickbox.com/v1/disposable';
 
+export const TX_METADATA_TIME = 300000;
 export const TX_METADATA_TYPES = ['likePay'];
--- src/routes/tx/tx.js
+++ src/routes/tx/tx.js
@@ -3,7 +3,10 @@ import { txCollection as txLogRef, userCollection } from '../../util/firebase';
 import { filterMultipleTxData } from '../../util/api/tx';
 import { filterTxData } from '../../util/ValidationHelper';
 import { jwtAuth } from '../../middleware/jwt';
-import { TX_METADATA_TYPES } from '../../constant';
+import {
+  TX_METADATA_TYPES,
+  TX_METADATA_TIME,
+} from '../../constant';
 
 const router = Router();
 
@@ -46,9 +49,19 @@ router.post('/id/:id/metadata', jwtAuth('write'), async (req, res, next) => {
     const {
       fromId,
       from,
+      ts,
+      metadata: docMetadata,
     } = txData;
     if (user !== fromId && from !== cosmosWallet) {
       res.sendStatus(403);
+      return;
+    }
+    if (docMetadata) {
+      res.status(419).send('CONFLICT');
+      return;
+    }
+    if (ts < Date.now() - TX_METADATA_TIME) {
+      res.status(400).send('EXPIRED');
       return;
     }
     if (Object.keys(metadata).some(m => !TX_METADATA_TYPES.includes(m))) {
