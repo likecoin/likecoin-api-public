db37cc42e3e4cef38bced57e9f175421792eb33f
William Chong <me@williamchong.cloud>
2022-01-07 01:26:09 +0800

    🔥 Remove iscn-dev signer api

--- src/routes/cosmos/iscn-dev/index.js
+++ src/routes/cosmos/iscn-dev/index.js
@@ -1,9 +1,7 @@
 import { Router } from 'express';
 import lcd from './lcd';
-import signer from './signer';
 
 const router = Router();
 router.use('/lcd', lcd);
-router.use('/signer', signer);
 
 export default router;
