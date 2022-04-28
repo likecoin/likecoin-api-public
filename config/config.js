b8b5768c5f63f8c9e008bd3d96079fa5177c452c
WeiJun0827 <weijunhung@outlook.com>
2022-04-28 17:03:12 +0800

    🧑‍💻 Set default chain config to improve dev exp

--- config/config.js
+++ config/config.js
@@ -12,12 +12,12 @@ config.FIRESTORE_COUPON_ROOT = process.env.FIRESTORE_COUPON_ROOT;
 config.FIRESTORE_OAUTH_CLIENT_ROOT = process.env.FIRESTORE_OAUTH_CLIENT_ROOT;
 config.FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
 
-config.COSMOS_LCD_ENDPOINT = 'localhost:1317';
-config.COSMOS_RPC_ENDPOINT = 'localhost:26657';
-config.COSMOS_CHAIN_ID = 'likechain-cosmos-testnet-2';
+config.COSMOS_LCD_ENDPOINT = 'https://node.testnet.like.co';
+config.COSMOS_RPC_ENDPOINT = 'https://node.testnet.like.co/rpc/';
+config.COSMOS_CHAIN_ID = 'likecoin-public-testnet-5';
 config.ISCN_DEV_LCD_ENDPOINT = 'localhost:1317';
 config.ISCN_DEV_CHAIN_ID = 'iscn-dev-chain';
-config.COSMOS_DENOM = 'nanolike';
+config.COSMOS_DENOM = 'nanoekil';
 
 config.ARWEAVE_LIKE_TARGET_ADDRESS = '';
 config.IPFS_ENDPOINT = 'https://ipfs.infura.io:5001/api/v0';
