a1da27c6a908dcb24afa4030f573fcf481de34ad
hsueh kuan tsai <b123456785678@gmail.com>
2022-06-01 17:33:37 +0800

    ♻️ Add test for estimation

--- test/stub/config/config.js
+++ test/stub/config/config.js
@@ -1,7 +1,8 @@
 const config = {};
 
-config.COSMOS_LCD_ENDPOINT = 'localhost:1317';
-config.COSMOS_RPC_ENDPOINT = 'localhost:26657';
+config.COSMOS_LCD_ENDPOINT = 'https://node.testnet.like.co';
+config.COSMOS_RPC_ENDPOINT = 'https://node.testnet.like.co/rpc/';
 config.ISCN_DEV_LCD_ENDPOINT = 'localhost:1317';
+config.IPFS_ENDPOINT = 'https://ipfs.infura.io:5001/api/v0';
 
 module.exports = config;
