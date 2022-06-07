e119b729d63e7bf587596887e2d9a1d06d1bdd23
William Chong <me@williamchong.cloud>
2022-06-07 16:24:05 +0800

    ✅ Fix gas fee change due to iscnjs update

--- test/api/iscn.test.js
+++ test/api/iscn.test.js
@@ -49,7 +49,7 @@ test('estimation: new', async (t) => {
   })
     .catch(err => err.response);
   t.is(res.status, 200);
-  t.is(res.data.LIKE, 0.17132514);
+  t.is(res.data.LIKE, 0.01231472);
 });
 
 test('estimation: upload', async (t) => {
@@ -98,5 +98,5 @@ test('estimation: upload', async (t) => {
     })
     .catch(err => err.response);
   t.is(res.status, 200);
-  t.is(res.data.LIKE, 3.17212514);
+  t.is(res.data.LIKE, 3.01311472);
 });
