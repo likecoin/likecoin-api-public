864b49e54a8c98cce845490686f6780f18acf036
hsueh kuan tsai <b123456785678@gmail.com>
2022-07-14 14:01:27 +0800

    🚧 Remove get image test

--- test/api/likernft-metadata.test.js
+++ test/api/likernft-metadata.test.js
@@ -1,8 +1,8 @@
-import test from 'ava';
-import axiosist from './axiosist';
+// import test from 'ava';
+// import axiosist from './axiosist';
 
 // const ISCN_ID_PREFIX = 'iscn://likecoin-chain/jDIU6eXjSttrEUvIPfvZZaMeGB6ckGOGX0EL4UYGraU';
-const CLASS_ID = 'likenft1rjmx2tr3fxj5gylnems3yz2y9hmz9k6vdjfewzav86cqu3gp3vaq83zw56';
+// const CLASS_ID = 'likenft1rjmx2tr3fxj5gylnems3yz2y9hmz9k6vdjfewzav86cqu3gp3vaq83zw56';
 // const NFT_ID_1 = 'liker-00027b31-b5a7-429b-84c8-b25a2e45c173';
 // const NFT_ID_2 = 'liker-0030e3e2-f5f9-40c7-8649-833e61a9bc35';
 
@@ -28,9 +28,9 @@ const CLASS_ID = 'likenft1rjmx2tr3fxj5gylnems3yz2y9hmz9k6vdjfewzav86cqu3gp3vaq83
 //   t.is(res2.data.iscnId, ISCN_ID_PREFIX);
 // });
 
-test('likernft: get nft image', async (t) => {
-  const res = await axiosist.get(`/api/likernft/metadata/image/class_${CLASS_ID}.png`)
-    .catch(err => err.response);
-  t.is(res.status, 200);
-  t.is(res.headers['content-type'], 'image/png');
-});
+// test('likernft: get nft image', async (t) => {
+//   const res = await axiosist.get(`/api/likernft/metadata/image/class_${CLASS_ID}.png`)
+//     .catch(err => err.response);
+//   t.is(res.status, 200);
+//   t.is(res.headers['content-type'], 'image/png');
+// });
