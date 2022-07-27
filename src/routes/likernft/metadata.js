7bd11c1879eddbfb1ce1fd7f2ecdbbca0dd8d1b7
Ng Wing Tat, David <i@ngwingt.at>
2022-07-28 01:29:25 +0800

    🚧 Do not apply image mask for NFT

--- src/routes/likernft/metadata.js
+++ src/routes/likernft/metadata.js
@@ -5,7 +5,7 @@ import { likeNFTCollection, iscnInfoCollection } from '../../util/firebase';
 import { filterLikeNFTMetadata } from '../../util/ValidationHelper';
 import { getISCNIdByClassId } from '../../util/api/likernft';
 import {
-  getLikerNFTDynamicData, getBasicImage, getCombinedImage, getResizedImage,
+  getLikerNFTDynamicData, getBasicImage, /* getCombinedImage, */ getResizedImage,
 } from '../../util/api/likernft/metadata';
 import { getNFTISCNData, getNFTClassDataById, getNFTOwner } from '../../util/cosmos/nft';
 import { fetchISCNIdAndClassId } from '../../middleware/likernft';
@@ -105,9 +105,13 @@ router.get(
       }
       const basicImage = await getBasicImage(image, title);
       const resizedImage = getResizedImage();
-      const combinedImage = await getCombinedImage();
+      // Disable image mask for now
+      // const combinedImage = await getCombinedImage();
       res.set('Cache-Control', `public, max-age=${60}, s-maxage=${60}, stale-if-error=${ONE_DAY_IN_S}`);
-      basicImage.pipe(resizedImage).pipe(combinedImage).pipe(res);
+      basicImage
+        .pipe(resizedImage)
+        // .pipe(combinedImage)
+        .pipe(res);
       return;
     } catch (err) {
       next(err);
