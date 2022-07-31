484d2fafbd96fcc1a1f5196b01a4cf8f2f664b6a
William Chong <me@williamchong.cloud>
2022-08-01 00:19:05 +0800

    ♻️ Refactor get iscn prefix function to throw error

--- src/util/cosmos/iscn.js
+++ src/util/cosmos/iscn.js
@@ -54,8 +54,8 @@ export async function getISCNSigningAddressInfo() {
 }
 
 export function getISCNPrefix(input) {
-  const res = /^(iscn:\/\/likecoin-chain\/[A-Za-z0-9-_]+)\/[0-9]+$/.exec(input);
-  if (!res) return input;
+  const res = /^(iscn:\/\/likecoin-chain\/[A-Za-z0-9-_]+)(?:\/([0-9]*))?$/.exec(input);
+  if (!res) throw new Error(`Invalid ISCN ID ${input}`);
   const [, prefix] = res;
   return prefix;
 }
