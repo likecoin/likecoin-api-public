9ee96786a2ccb376636aba95f089bfaeba9917bb
hsueh kuan tsai <b123456785678@gmail.com>
2022-08-11 20:11:54 +0800

    ♻️ Fix ipfs endpoint in test config

--- test/stub/config/config.js
+++ test/stub/config/config.js
@@ -4,6 +4,6 @@ config.COSMOS_LCD_ENDPOINT = 'https://node.testnet.like.co';
 config.COSMOS_RPC_ENDPOINT = 'https://node.testnet.like.co/rpc/';
 config.COSMOS_SIGNING_RPC_ENDPOINT = 'https://node.testnet.like.co/rpc/';
 config.ISCN_DEV_LCD_ENDPOINT = 'localhost:1317';
-config.IPFS_ENDPOINT = 'https://ipfs.infura.io:5001/api/v0';
+config.IPFS_ENDPOINT = 'https://ipfs.io:5001/api/v0';
 
 module.exports = config;
