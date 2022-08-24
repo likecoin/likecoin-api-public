92ccf34b1cd78a452e145e05751a57994d06dff4
WeiJun0827 <weijunhung@outlook.com>
2022-08-24 18:42:59 +0800

    🔥 Remove unused /user/:wallet/own endpoint

--- src/routes/likernft/user.js
+++ src/routes/likernft/user.js
@@ -7,25 +7,6 @@ import { ValidationError } from '../../util/ValidationError';
 const router = Router();
 
 router.get(
-  '/user/:wallet/own',
-  async (req, res, next) => {
-    try {
-      const { wallet } = req.params;
-      if (!isValidLikeAddress(wallet)) throw new ValidationError('INVALID_WALLET');
-      // TODO: swap to chain based query
-      const query = await db.collectionGroup('nft').where('ownerWallet', '==', wallet).get();
-      const list = query.docs.map(d => d.data().classId);
-      const uniqueList = Array.from(new Set(list));
-      res.json({
-        list: uniqueList,
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
-);
-
-router.get(
   '/user/:wallet/sell',
   async (req, res, next) => {
     try {
