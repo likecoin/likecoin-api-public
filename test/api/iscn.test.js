6dec87b5b0d9c08983253cb5e79ccc1b43f05f8c
hsueh kuan tsai <b123456785678@gmail.com>
2022-10-06 09:32:26 +0800

    ♻️ Update test LIKE

--- test/api/iscn.test.js
+++ test/api/iscn.test.js
@@ -49,7 +49,7 @@ test('estimation: new', async (t) => {
   })
     .catch(err => err.response);
   t.is(res.status, 200);
-  t.is(res.data.LIKE, 0.01231472);
+  t.is(res.data.LIKE, 0.01231446);
 });
 
 // test('estimation: upload', async (t) => {
