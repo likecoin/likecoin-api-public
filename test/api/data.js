427b8c10be4cdcbb2d8480b98071c2d162710b36
William Chong <me@williamchong.cloud>
2022-10-25 02:55:50 +0800

    ♻️ Refactor test key location

--- test/api/data.js
+++ test/api/data.js
@@ -6,6 +6,9 @@ const subscriptionData = require('../data/subscription.json');
 // test data
 //
 export const testingWallet0 = '0x4aa15ea87D6fFb649343E3daBb92f244f3327518';
+export const testingCosmosWallet0 = 'cosmos187290tx4vj6npyl7fdfgdvxr2n9d5qyell50d4';
+export const testingLikeWallet0 = 'like187290tx4vj6npyl7fdfgdvxr2n9d5qyevrgdww';
+
 export const {
   id: testingUser1,
   displayName: testingDisplayName1,
@@ -82,3 +85,7 @@ export const privateKey0 = '0xbad2b5497cf7f9f3938990cb17e5b4f6f2073e435f43b5c17e
 export const privateKey1 = '0x3b298aeb848c19257e334160b52aae9790fbae9607bd68aea8cfcfc89572cb15';
 export const privateKey2 = '0x8163e9a0e9ec131844c520d292380bd93f39fd45d1bbce5c8ae3d2a4ef0a702b';
 export const privateKey3 = '0xd9d199217049b92cb321d3e636b1d6642d89af0cef08b56d61bf04467b4d3862';
+
+
+export const cosmosPrivateKeyNew = '6a47b2c6557573c1e4dd82563c64a6db3abefad4ea722093b4eeec204ebd9a3a';
+export const cosmosPrivateKeyDelete = 'e5c510dfa57564a887ad76bbed56dae555b3a92fd37842aaf96cb29e14242bb3';
