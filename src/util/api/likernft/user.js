6574f617691840bf7c69b39e1843436167299336
WeiJun0827 <weijunhung@outlook.com>
2022-11-03 23:25:55 +0800

    ✨ Add `GET /user/:wallet/stats` endpoint

--- src/util/api/likernft/user.js
+++ src/util/api/likernft/user.js
@@ -1,5 +1,8 @@
-import { db } from '../../firebase';
+import axios from 'axios';
+import { db, likeNFTCollection } from '../../firebase';
 import { getNFTISCNOwner } from '../../cosmos/nft';
+import { FIRESTORE_IN_QUERY_LIMIT } from '../../../constant';
+import { COSMOS_LCD_INDEXER_ENDPOINT } from '../../../../config/config';
 
 const BATCH_SIZE = 200;
 
@@ -34,4 +37,36 @@ export async function filterOwnedClassIds(iscnDocs, wallet) {
   return Array.from(classIdSet);
 }
 
-export default filterOwnedClassIds;
+export async function getUserStat(wallet) {
+  const { data: userStat } = await axios.get(`${COSMOS_LCD_INDEXER_ENDPOINT}/likechain/likenft/v1/user-stat?user=${wallet}`);
+  const {
+    collected_classes: collectedClasses,
+    created_count: createdClassCount,
+    collector_count: createdCollectorCount,
+  } = userStat;
+  const collectedClassCount = collectedClasses.length;
+  const collectedClassIds = collectedClasses.map(c => c.class_id);
+  const arrays = [];
+  for (let i = 0; i < collectedClassIds.length; i += FIRESTORE_IN_QUERY_LIMIT) {
+    arrays.push(collectedClassIds.slice(i, i + FIRESTORE_IN_QUERY_LIMIT));
+  }
+  const queries = await Promise.all(arrays.map(classIds => likeNFTCollection.where('classId', 'in', classIds).get()));
+  const docs = queries.reduce((acc, q) => acc.concat(q.docs), []);
+
+  const priceMap = {};
+  docs.forEach((doc) => {
+    const { classId, currentPrice } = doc.data();
+    priceMap[classId] = currentPrice;
+  });
+
+  const collectedNftValue = collectedClasses.reduce((acc, c) => {
+    const price = priceMap[c.class_id];
+    return price ? acc + price * c.count : acc;
+  }, 0);
+  return {
+    collectedClassCount,
+    collectedNftValue,
+    createdClassCount,
+    createdCollectorCount,
+  };
+}
