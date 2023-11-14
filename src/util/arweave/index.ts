2a7a174ea4857fa5a3d597e6ae83770c069bcd93
William Chong <me@williamchong.cloud>
2023-11-14 21:29:35 +0800

    🥅 Use a more sensible fallback for matic/like price

--- src/util/arweave/index.ts
+++ src/util/arweave/index.ts
@@ -211,7 +211,7 @@ async function getMATICPriceRatioBigNumber() {
     // eslint-disable-next-line no-console
     console.error(JSON.stringify(err));
     // TODO: make a less hardcoded fallback price
-    return new BigNumber(5000);
+    return new BigNumber(700);
   }
 }
 
