1c9101d8b3b4bbb52e3f8a762b3e2cff0a48caa9
William Chong <me@williamchong.cloud>
2024-09-06 01:35:40 +0800

    ✨ Add stripe product and price on new book listing

--- src/routes/likernft/book/store.ts
+++ src/routes/likernft/book/store.ts
@@ -515,6 +515,7 @@ router.post(['/:classId/new', '/class/:classId/new'], jwtAuth('write:nftbook'), 
       isbn,
     } = iscnContentMetadata;
     const keywords = keywordString.split(',').map((k: string) => k.trim()).filter((k: string) => !!k);
+    const image = metadata?.data?.metadata?.image;
 
     await newNftBookInfo(classId, {
       iscnIdPrefix,
@@ -541,6 +542,7 @@ router.post(['/:classId/new', '/class/:classId/new'], jwtAuth('write:nftbook'), 
       author,
       usageInfo,
       isbn,
+      image,
     }, apiWalletOwnedNFTIds);
 
     const className = metadata?.name || classId;
@@ -566,7 +568,7 @@ router.post(['/:classId/new', '/class/:classId/new'], jwtAuth('write:nftbook'), 
         type: metadata?.data?.metadata?.nft_meta_collection_id,
         minPrice: prices.reduce((min, p) => Math.min(min, p.priceInDecimal), Infinity) / 100,
         maxPrice: prices.reduce((max, p) => Math.max(max, p.priceInDecimal), 0) / 100,
-        imageURL: metadata?.data?.metadata?.image,
+        imageURL: image,
         language: inLanguage,
         keywords,
         author,
