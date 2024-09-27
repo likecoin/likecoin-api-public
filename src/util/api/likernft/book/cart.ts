d719600090da138c0699413a89c371c95b671691
William Chong <me@williamchong.cloud>
2024-09-28 02:52:12 +0800

    ✨ Use stripe price id if exists on book checkout

--- src/util/api/likernft/book/cart.ts
+++ src/util/api/likernft/book/cart.ts
@@ -74,6 +74,7 @@ export type CartItemWithInfo = CartItem & {
   priceIndex?: number,
   iscnPrefix?: string,
   priceName?: string,
+  stripePriceId?: string,
   quantity: number,
 }
 
@@ -743,6 +744,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
         isAllowCustomPrice,
         name: priceNameObj,
         description: pricDescriptionObj,
+        stripePriceId,
       } = prices[priceIndex];
       let { name = '', description = '' } = metadata;
       const classMetadata = metadata.data.metadata;
@@ -775,13 +777,14 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
         classId,
         iscnPrefix,
         priceName,
+        stripePriceId,
       };
     } else if (collectionId) {
       const collectionData = await getBookCollectionInfoById(collectionId);
       if (!collectionData) throw new ValidationError('NFT_NOT_FOUND');
-      const { classIds } = collectionData;
-      const { image } = collectionData;
       const {
+        classIds,
+        image,
         ownerWallet,
         shippingRates,
         isPhysicalOnly,
@@ -792,6 +795,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
         hasShipping,
         name: collectionNameObj,
         description: collectionDescriptionObj,
+        stripePriceId,
       } = collectionData;
       const classDataList = await Promise.all(classIds.map((id) => getNFTClassDataById(id)));
 
@@ -818,6 +822,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
         isLikerLandArt,
         originalPriceInDecimal,
         collectionId,
+        stripePriceId,
       };
     } else {
       throw new ValidationError('ITEM_ID_NOT_SET');
@@ -837,6 +842,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
       shippingRates,
       isLikerLandArt,
       priceName = '',
+      stripePriceId,
     } = info;
 
     if (hasShipping) throw new ValidationError('CART_ITEM_HAS_SHIPPING');
@@ -880,6 +886,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
       classId,
       priceIndex,
       quantity,
+      stripePriceId,
     };
   }));
 
@@ -961,6 +968,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
       collectionId,
       priceIndex,
       iscnPrefix,
+      stripePriceId,
       from: itemFrom,
     } = info;
     return {
@@ -976,6 +984,7 @@ export async function handleNewCartStripeCheckout(items: CartItem[], {
       collectionId,
       priceIndex,
       iscnPrefix,
+      stripePriceId,
       from: itemFrom,
     };
   }), {
