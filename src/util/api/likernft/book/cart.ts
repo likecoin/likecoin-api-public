214c99b73724a1e7e04aa3fcf0c403027d4110f9
William Chong <me@williamchong.cloud>
2024-10-10 15:03:14 +0800

    🐛 Fix commission discount amount should exclude tip

--- src/util/api/likernft/book/cart.ts
+++ src/util/api/likernft/book/cart.ts
@@ -26,6 +26,7 @@ import {
   claimNFTBook,
   calculateFeeAndDiscountFromBalanceTx,
   DISCOUNTED_FEE_TYPES,
+  calculateCommissionWithDiscount,
 } from './purchase';
 import {
   db,
@@ -335,7 +336,6 @@ async function updateNFTBookCartPostCheckoutFeeInfo({
     newFeeInfo,
     shippingCostAmount,
     discountRate,
-    discountAmount,
   } = calculateFeeAndDiscountFromBalanceTx({
     paymentId,
     amountSubtotal,
@@ -354,7 +354,6 @@ async function updateNFTBookCartPostCheckoutFeeInfo({
     ...newFeeInfo,
     stripeFeeCurrency,
     discountRate,
-    discountAmount,
     isAmountFeeUpdated,
     isStripeFeeUpdated,
   };
@@ -410,7 +409,6 @@ export async function processNFTBookCartStripePurchase(
     const {
       stripeFeeAmount: totalStripeFeeAmount,
       stripeFeeCurrency,
-      discountAmount,
       discountRate,
       isAmountFeeUpdated,
       isStripeFeeUpdated,
@@ -464,7 +462,6 @@ export async function processNFTBookCartStripePurchase(
       const prediscountTotal = amountSubtotal || totalFeeInfo.priceInDecimal;
       const stripeFeeAmount = Math.ceil((totalStripeFeeAmount * priceInDecimal)
         / prediscountTotal) || documentStripeFeeAmount;
-      const discountAmountForItem = Math.ceil((discountAmount * priceInDecimal) / prediscountTotal);
       const feeInfo: TransactionFeeInfo = {
         ...docFeeInfo,
       };
@@ -475,19 +472,19 @@ export async function processNFTBookCartStripePurchase(
           }
         });
         if (channelCommission) {
-          feeInfo.channelCommission -= discountAmountForItem;
-          if (feeInfo.channelCommission < 0) {
-            feeInfo.channelCommission = 0;
-            // eslint-disable-next-line no-console
-            console.error(`Negative channel commission in cart ${cartId} for item ${classId || collectionId}`);
-          }
+          feeInfo.channelCommission = calculateCommissionWithDiscount({
+            paymentId: `${paymentId}-${itemIndex}`,
+            discountRate,
+            originalPriceInDecimal,
+            commission: channelCommission,
+          });
         } else if (likerLandCommission) {
-          feeInfo.likerLandCommission -= discountAmountForItem;
-          if (feeInfo.likerLandCommission < 0) {
-            feeInfo.likerLandCommission = 0;
-            // eslint-disable-next-line no-console
-            console.error(`Negative likerLand commission in cart ${cartId} for item ${classId || collectionId}`);
-          }
+          feeInfo.likerLandCommission = calculateCommissionWithDiscount({
+            paymentId: `${paymentId}-${itemIndex}`,
+            discountRate,
+            originalPriceInDecimal,
+            commission: likerLandCommission,
+          });
         } else {
           // eslint-disable-next-line no-console
           console.error(`No commission found but discounted in cart ${cartId} for item ${classId || collectionId}`);
--- src/util/api/likernft/book/purchase.ts
+++ src/util/api/likernft/book/purchase.ts
@@ -1216,6 +1216,22 @@ export const DISCOUNTED_FEE_TYPES = [
   'customPriceDiff',
 ];
 
+export function calculateCommissionWithDiscount({
+  paymentId,
+  commission,
+  originalPriceInDecimal,
+  discountRate,
+}) {
+  const originalRate = commission / originalPriceInDecimal;
+  const discountedRate = originalRate - (1 - discountRate);
+  if (discountedRate < 0) {
+    // eslint-disable-next-line no-console
+    console.error(`Negative commission rate ${discountedRate} for paymentId: ${paymentId}`);
+    return 0;
+  }
+  return Math.floor(originalPriceInDecimal * discountedRate);
+}
+
 export function calculateFeeAndDiscountFromBalanceTx({
   paymentId,
   amountSubtotal,
@@ -1229,6 +1245,7 @@ export function calculateFeeAndDiscountFromBalanceTx({
     channelCommission,
     likerLandCommission,
     priceInDecimal,
+    originalPriceInDecimal,
   } = feeInfo as TransactionFeeInfo;
   const stripeFeeDetails = balanceTx.fee_details.find((fee) => fee.type === 'stripe_fee');
   const stripeFeeCurrency = stripeFeeDetails?.currency || 'USD';
@@ -1240,7 +1257,8 @@ export function calculateFeeAndDiscountFromBalanceTx({
   const isAmountFeeUpdated = priceInDecimal !== productAmountTotal
     && productAmountTotal !== amountSubtotal;
   const discountRate = isAmountFeeUpdated ? (productAmountTotal / amountSubtotal) : 1;
-  const discountAmount = amountSubtotal - productAmountTotal;
+  const totalDiscountAmount = amountSubtotal - productAmountTotal;
+  const originalPriceDiscountAmount = Math.ceil(discountRate * originalPriceInDecimal);
   if (isAmountFeeUpdated) {
     DISCOUNTED_FEE_TYPES.forEach((key) => {
       if (typeof newFeeInfo[key] === 'number') {
@@ -1248,28 +1266,29 @@ export function calculateFeeAndDiscountFromBalanceTx({
       }
     });
     if (channelCommission) {
-      newFeeInfo.channelCommission -= discountAmount;
-      if (newFeeInfo.channelCommission < 0) {
-        newFeeInfo.channelCommission = 0;
-        // eslint-disable-next-line no-console
-        console.error(`Negative channelCommission for paymentId: ${paymentId}`);
-      }
+      newFeeInfo.channelCommission = calculateCommissionWithDiscount({
+        paymentId,
+        commission: channelCommission,
+        originalPriceInDecimal,
+        discountRate,
+      });
     } else if (likerLandCommission) {
-      newFeeInfo.likerLandCommission -= discountAmount;
-      if (newFeeInfo.likerLandCommission < 0) {
-        newFeeInfo.likerLandCommission = 0;
-        // eslint-disable-next-line no-console
-        console.error(`Negative likerLandCommission for paymentId: ${paymentId}`);
-      }
+      newFeeInfo.likerLandCommission = calculateCommissionWithDiscount({
+        paymentId,
+        commission: likerLandCommission,
+        originalPriceInDecimal,
+        discountRate,
+      });
     } else {
       // eslint-disable-next-line no-console
-      console.error(`Discount amount ${discountAmount} but no commission found for paymentId: ${paymentId}`);
+      console.error(`Discount amount ${totalDiscountAmount} but no commission found for paymentId: ${paymentId}`);
     }
   }
   return {
     newFeeInfo,
     stripeFeeCurrency,
-    discountAmount,
+    totalDiscountAmount,
+    originalPriceDiscountAmount,
     discountRate,
     shippingCostAmount,
     isStripeFeeUpdated,
