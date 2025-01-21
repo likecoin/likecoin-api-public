c5c9f014d162f1c9ae5efa07f386565f956a02c2
William Chong <me@williamchong.cloud>
2025-01-22 01:02:58 +0800

    ✨ Add evm migration API

--- src/routes/wallet/index.ts
+++ src/routes/wallet/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { checkCosmosSignPayload, checkEVMSignPayload } from '../../util/api/users';
+import { checkCosmosSignPayload, checkEVMSignPayload, getUserWithCivicLikerPropertiesByWallet } from '../../util/api/users';
 import { ValidationError } from '../../util/ValidationError';
 import { jwtSign } from '../../util/jwt';
-import { findLikeWalletByEVMWallet } from '../../util/api/wallet';
+import {
+  findLikeWalletByEVMWallet,
+  checkBookUserEVMWallet,
+  migrateBookClassId,
+  migrateLikeWalletToEVMWallet,
+} from '../../util/api/wallet';
 import publisher from '../../util/gcloudPub';
 import { PUBSUB_TOPIC_MISC } from '../../constant';
-import { isValidLikeAddress } from '../../util/cosmos';
+import { checkAddressValid, checkCosmosAddressValid } from '../../util/ValidationHelper';
 
 const router = Router();
 
@@ -26,7 +31,7 @@ router.post('/authorize', async (req, res, next) => {
       signed = checkEVMSignPayload({
         signature, message, inputWallet, signMethod, action: 'authorize',
       });
-    } else if (isValidLikeAddress(inputWallet)) {
+    } else if (checkCosmosAddressValid(inputWallet, 'like')) {
       if (!publicKey) throw new ValidationError('INVALID_PAYLOAD');
       signed = checkCosmosSignPayload({
         signature, publicKey, message, inputWallet, signMethod, action: 'authorize',
@@ -61,6 +66,129 @@ router.post('/authorize', async (req, res, next) => {
     });
 
     res.json({ jwtid, token });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/evm/migrate/book', async (req, res, next) => {
+  try {
+    const {
+      like_class_id: likeClassId,
+      evm_class_id: evmClassId,
+    } = req.body;
+    if (!likeClassId || !evmClassId) throw new ValidationError('INVALID_PAYLOAD');
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'migrateBookClassIdBegin',
+      likeClassId,
+      evmClassId,
+    });
+    const {
+      error,
+      migratedClassIds,
+      migratedCollectionIds,
+    } = await migrateBookClassId(likeClassId, evmClassId);
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'migrateBookClassIdEnd',
+      likeClassId,
+      evmClassId,
+      migratedClassIds,
+      migratedCollectionIds,
+      error,
+    });
+    res.json({
+      migratedClassIds,
+      migratedCollectionIds,
+      error,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/evm/migrate/user/addr/:likeWallet', async (req, res, next) => {
+  try {
+    const { likeWallet } = req.params;
+    if (!likeWallet || !checkCosmosAddressValid(likeWallet, 'like')) {
+      throw new ValidationError('INVALID_PAYLOAD');
+    }
+    const [likerIdInfo, evmWallet] = await Promise.all([
+      getUserWithCivicLikerPropertiesByWallet(likeWallet),
+      checkBookUserEVMWallet(likeWallet),
+    ]);
+    res.json({ likerIdInfo, evmWallet });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post(['/evm/migrate/user', '/evm/migrate/all'], async (req, res, next) => {
+  try {
+    const {
+      cosmos_address: likeWallet,
+      cosmos_signature: signature,
+      cosmos_public_key: publicKey,
+      cosmos_signature_content: message, signMethod,
+    } = req.body;
+    if (!likeWallet || !signature || !publicKey || !message) throw new ValidationError('INVALID_PAYLOAD');
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'migrateLikeUserToEVMUserRequested',
+      likeWallet,
+    });
+    const signed = checkCosmosSignPayload({
+      signature, publicKey, message, inputWallet: likeWallet, signMethod, action: 'migrate',
+    });
+    if (!signed) {
+      throw new ValidationError('INVALID_SIGN');
+    }
+    const { evm_wallet: evmWallet } = signed;
+    if (!evmWallet || !checkAddressValid(evmWallet)) {
+      throw new ValidationError('INVALID_PAYLOAD');
+    }
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'migrateLikeUserToEVMUserBegin',
+      likeWallet,
+      evmWallet,
+    });
+    const {
+      isMigratedBookUser,
+      isMigratedBookOwner,
+      isMigratedLikerId,
+      isMigratedLikerLand,
+      migratedLikerId,
+      migratedLikerLandUser,
+      migrateBookUserError,
+      migrateBookOwnerError,
+      migrateLikerIdError,
+      migrateLikerLandError,
+    } = await migrateLikeWalletToEVMWallet(likeWallet, evmWallet);
+    publisher.publish(PUBSUB_TOPIC_MISC, req, {
+      logType: 'migrateLikeWalletToEVMUserEnd',
+      likeWallet,
+      evmWallet,
+      isMigratedBookUser,
+      isMigratedBookOwner,
+      isMigratedLikerId,
+      isMigratedLikerLand,
+      migratedLikerId,
+      migratedLikerLandUser,
+      migrateBookUserError,
+      migrateBookOwnerError,
+      migrateLikerIdError,
+      migrateLikerLandError,
+    });
+    res.json({
+      isMigratedBookUser,
+      isMigratedBookOwner,
+      isMigratedLikerId,
+      isMigratedLikerLand,
+      migratedLikerId,
+      migratedLikerLandUser,
+      migrateBookUserError,
+      migrateBookOwnerError,
+      migrateLikerIdError,
+      migrateLikerLandError,
+    });
   } catch (err) {
     next(err);
   }
--- src/util/api/wallet/index.ts
+++ src/util/api/wallet/index.ts
@@ -1,5 +1,14 @@
 import { isValidLikeAddress } from '../../cosmos';
-import { likeNFTBookUserCollection } from '../../firebase';
+import { getNFTClassDataById, isEVMClassId } from '../../evm/nft';
+import {
+  db,
+  FieldValue,
+  likeNFTBookCollection,
+  likeNFTBookUserCollection,
+  likeNFTCollectionCollection,
+  userCollection,
+} from '../../firebase';
+import { migrateLikerLandEVMWallet } from '../../liker-land';
 
 export async function findLikeWalletByEVMWallet(evmWallet: string) {
   const userQuery = await likeNFTBookUserCollection.where('evmWallet', '==', evmWallet).get();
@@ -11,6 +20,262 @@ export async function findLikeWalletByEVMWallet(evmWallet: string) {
     return docId;
   }
   return null;
+}
+
+export async function checkBookUserEVMWallet(likeWallet: string) {
+  const userQuery = await likeNFTBookUserCollection.doc(likeWallet).get();
+  if (!userQuery.exists) {
+    return null;
+  }
+  return userQuery.data()?.evmWallet || null;
+}
+
+async function migrateBookUser(likeWallet: string, evmWallet: string) {
+  try {
+    const { userExists, alreadyMigrated } = await db.runTransaction(async (t) => {
+      const [userDoc, evmUserDoc] = await Promise.all([
+        t.get(likeNFTBookUserCollection.doc(likeWallet)),
+        t.get(likeNFTBookUserCollection.doc(evmWallet)),
+      ]);
+      if (evmUserDoc.exists) {
+        const {
+          likeWallet: evmLikeWallet,
+        } = evmUserDoc.data();
+        if (evmLikeWallet) {
+          if (evmLikeWallet !== likeWallet) {
+            throw new Error('EVM_WALLET_USED_BY_OTHER_USER');
+          } else {
+            return {
+              userExists: true,
+              alreadyMigrated: true,
+            };
+          }
+        }
+      }
+      if (!userDoc.exists) {
+        t.create(likeNFTBookUserCollection.doc(likeWallet), {
+          evmWallet,
+          migrateTimestamp: FieldValue.serverTimestamp(),
+          timestamp: FieldValue.serverTimestamp(),
+        });
+      } else {
+        const userData = userDoc.data();
+        const { evmWallet: existingEVMWallet } = userData;
+        if (existingEVMWallet && existingEVMWallet !== evmWallet) {
+          throw new Error('EVM_WALLET_NOT_MATCH_USER_RECORD');
+        }
+        t.update(userDoc.ref, {
+          evmWallet,
+          migrateTimestamp: FieldValue.serverTimestamp(),
+        });
+      }
+      if (!evmUserDoc.exists) {
+        t.create(likeNFTBookUserCollection.doc(evmWallet), {
+          likeWallet,
+          migrateTimestamp: FieldValue.serverTimestamp(),
+          timestamp: FieldValue.serverTimestamp(),
+        });
+      } else {
+        t.update(likeNFTBookUserCollection.doc(evmWallet), {
+          likeWallet,
+          migrateTimestamp: FieldValue.serverTimestamp(),
+        });
+      }
+      return {
+        userExists: userDoc.exists,
+        alreadyMigrated: false,
+      };
+    });
+    return { error: null, userExists, alreadyMigrated };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return { error: (error as Error).message };
+  }
+}
+
+async function migrateBookOwner(likeWallet: string, evmWallet: string) {
+  try {
+    await db.runTransaction(async (t) => {
+      const bookQuery = await t.get(likeNFTBookCollection.where('ownerWallet', '==', likeWallet));
+      bookQuery.docs.forEach((doc) => {
+        t.update(doc.ref, { ownerWallet: evmWallet });
+      });
+    });
+    await db.runTransaction(async (t) => {
+      const bookQuery = await t.get(likeNFTBookCollection.where(`connectedWallets.${likeWallet}`, '>', 0));
+      bookQuery.docs.forEach((doc) => {
+        const {
+          connectedWallets,
+        } = doc.data();
+        const ratio = connectedWallets[likeWallet];
+        delete connectedWallets[likeWallet];
+        connectedWallets[evmWallet] = ratio;
+        t.update(doc.ref, { connectedWallets });
+      });
+    });
+    return { error: null };
+  } catch (error) {
+  // eslint-disable-next-line no-console
+    console.error(error);
+    return { error: (error as Error).message };
+  }
+}
+
+async function migrateLikerId(likeWallet:string, evmWallet: string) {
+  try {
+    const likerId = await db.runTransaction(async (t) => {
+      const [evmQuery, userQuery] = await Promise.all([
+        t.get(userCollection.where('evmWallet', '==', evmWallet).limit(1)),
+        t.get(userCollection.where('likeWallet', '==', likeWallet).limit(1)),
+      ]);
+      if (evmQuery.docs.length > 0) {
+        throw new Error('EVM_WALLET_ALREADY_EXIST');
+      }
+      if (userQuery.docs.length === 0) {
+        throw new Error('LIKE_WALLET_NOT_FOUND');
+      }
+      const userDoc = userQuery.docs[0];
+      t.update(userDoc.ref, { evmWallet });
+      return userDoc.id;
+    });
+    return { likerId, error: null };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return { likerId: null, error: (error as Error).message };
+  }
+}
+
+export async function migrateBookClassId(likeClassId:string, evmClassId: string) {
+  try {
+    const evmData = await getNFTClassDataById(evmClassId);
+    if (evmData?.likecoin?.classId !== likeClassId) {
+      throw new Error('EVM_CLASS_ID_NOT_MATCH_LIKE_CLASS_ID');
+    }
+    const res = await db.runTransaction(async (t) => {
+      const migratedClassIds: string[] = [];
+      const migratedCollectionIds: string[] = [];
+      const [bookListingDoc, collectionQuery] = await Promise.all([
+        t.get(likeNFTBookCollection.doc(likeClassId)),
+        t.get(likeNFTCollectionCollection.where('classIds', 'array-contains', likeClassId).limit(100)),
+      ]);
+      if (bookListingDoc.exists) {
+        const { evmClassId: existingEVMClassId } = bookListingDoc.data();
+        if (!existingEVMClassId) {
+          t.update(bookListingDoc.ref, { evmClassId });
+          t.create(likeNFTBookCollection.doc(evmClassId), {
+            ...bookListingDoc.data(),
+            chain: 'evm',
+            likeClassId,
+            migrateTimestamp: FieldValue.serverTimestamp(),
+          });
+          migratedClassIds.push(likeClassId);
+        }
+      }
+      collectionQuery.docs.forEach((doc) => {
+        const { classIds } = doc.data();
+        const index = classIds.indexOf(likeClassId);
+        if (index >= 0) {
+          classIds[index] = evmClassId;
+          t.update(doc.ref, {
+            classIds,
+            chain: classIds.every((id) => isEVMClassId(id)) ? 'evm' : 'like',
+          });
+          migratedCollectionIds.push(doc.id);
+        }
+      });
+      return {
+        migratedClassIds,
+        migratedCollectionIds,
+      };
+    });
+    const {
+      migratedClassIds,
+      migratedCollectionIds,
+    } = res;
+    return {
+      error: null,
+      migratedClassIds,
+      migratedCollectionIds,
+    };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return { error: (error as Error).message };
+  }
+}
+
+export async function migrateLikeUserToEVMUser(likeWallet: string, evmWallet: string) {
+  const { error: migrateBookUserError } = await migrateBookUser(likeWallet, evmWallet);
+  if (migrateBookUserError) {
+    return {
+      isMigratedBookUser: false,
+      isMigratedLikerId: false,
+      isMigratedLikerLand: false,
+      migratedLikerId: null,
+      migratedLikerLandUser: null,
+      migrateBookUserError,
+      migrateLikerIdError: null,
+      migrateLikerLandError: null,
+    };
+  }
+  const [
+    { error: migrateLikerIdError, likerId },
+    { error: migrateLikerLandError, user: likerLandUser },
+  ] = await Promise.all([
+    migrateLikerId(likeWallet, evmWallet),
+    migrateLikerLandEVMWallet(likeWallet, evmWallet),
+  ]);
+  return {
+    isMigratedBookUser: !migrateBookUserError,
+    isMigratedLikerId: !migrateLikerIdError,
+    isMigratedLikerLand: !migrateLikerLandError,
+    migratedLikerId: likerId,
+    migratedLikerLandUser: likerLandUser,
+    migrateBookUserError,
+    migrateLikerIdError,
+    migrateLikerLandError,
+  };
+}
+
+export async function migrateLikeWalletToEVMWallet(likeWallet: string, evmWallet: string) {
+  const { error: migrateBookUserError } = await migrateBookUser(likeWallet, evmWallet);
+  if (migrateBookUserError) {
+    return {
+      isMigratedBookUser: false,
+      isMigratedBookOwner: false,
+      isMigratedLikerId: false,
+      isMigratedLikerLand: false,
+      migratedLikerId: null,
+      migratedLikerLandUser: null,
+      migrateBookUserError,
+      migrateBookOwnerError: null,
+      migrateLikerIdError: null,
+      migrateLikerLandError: null,
+    };
+  }
+  const [
+    { error: migrateBookOwnerError },
+    { error: migrateLikerIdError, likerId },
+    { error: migrateLikerLandError, user: likerLandUser },
+  ] = await Promise.all([
+    migrateBookOwner(likeWallet, evmWallet),
+    migrateLikerId(likeWallet, evmWallet),
+    migrateLikerLandEVMWallet(likeWallet, evmWallet),
+  ]);
+  return {
+    isMigratedBookUser: !migrateBookUserError,
+    isMigratedBookOwner: !migrateBookOwnerError,
+    isMigratedLikerId: !migrateLikerIdError,
+    isMigratedLikerLand: !migrateLikerLandError,
+    migratedLikerId: likerId,
+    migratedLikerLandUser: likerLandUser,
+    migrateBookUserError,
+    migrateBookOwnerError,
+    migrateLikerIdError,
+    migrateLikerLandError,
+  };
 }
 
 export default findLikeWalletByEVMWallet;
