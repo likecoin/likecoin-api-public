cc4ba17d1bc284da8f23f6e0ad249dd8c7531f1b
William Chong <me@williamchong.cloud>
2025-03-03 20:28:55 +0800

    ♻️ Refactor shipping logic

---
+++ src/util/api/likernft/book/type.ts
@@ -0,0 +1,31 @@
+export type CartItem = {
+  collectionId?: string
+  classId?: string
+  priceIndex?: number
+  customPriceInDecimal?: number
+  quantity?: number
+  from?: string
+}
+
+export type CartItemWithInfo = CartItem & {
+  priceInDecimal: number;
+  customPriceDiffInDecimal: number;
+  stock: number;
+  hasShipping: boolean;
+  isPhysicalOnly: boolean;
+  isAllowCustomPrice: boolean;
+  name: string,
+  description: string,
+  images: string[],
+  ownerWallet: string,
+  shippingRates: any[],
+  isLikerLandArt: boolean;
+  originalPriceInDecimal: number,
+  collectionId?: string,
+  classId?: string,
+  priceIndex?: number,
+  iscnPrefix?: string,
+  priceName?: string,
+  stripePriceId?: string,
+  quantity: number,
+}
