09d1b17372c670fc31c07f3341d36e2b2b58f97c
William Chong <me@williamchong.cloud>
2025-03-05 01:26:04 +0800

    🚧 Add basic auto deliver evm logic

--- src/util/api/likernft/book/index.ts
+++ src/util/api/likernft/book/index.ts
@@ -388,7 +388,7 @@ export async function updateNftBookInfo(classId: string, {
   const classIdRef = likeNFTBookCollection.doc(classId);
   let batch = db.batch();
   batch.update(classIdRef, payload);
-  if (newAPIWalletOwnedNFTIds.length) {
+  if (newAPIWalletOwnedNFTIds?.length) {
     for (let i = 0; i < newAPIWalletOwnedNFTIds.length; i += 1) {
       if ((i + 1) % FIRESTORE_BATCH_SIZE === 0) {
         // eslint-disable-next-line no-await-in-loop
@@ -611,6 +611,10 @@ export async function validateAutoDeliverNFTsTxHash(
   sender: string,
   expectedNFTCount: number,
 ) {
+  if (isEVMClassId(classId)) {
+    // evm auto deliver nfts are minted on demand
+    return [];
+  }
   const nftIdsMap = await parseNFTIdsMapFromTxHash(txHash, sender);
   const nftIds = nftIdsMap[classId];
   if (!nftIds) {
