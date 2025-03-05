aa3141b81a0bb33b3c017d90d1f923c3876e7c39
William Chong <me@williamchong.cloud>
2025-03-05 16:02:19 +0800

    🐛 Fix iscnIdPrefix is undefined

--- src/util/api/likernft/book/index.ts
+++ src/util/api/likernft/book/index.ts
@@ -306,7 +306,8 @@ export async function syncNFTBookInfoWithISCN(classId) {
   } = bookInfo;
   const keywords = keywordString.split(',').map((k: string) => k.trim()).filter((k: string) => !!k);
 
-  const payload: any = { iscnIdPrefix };
+  const payload: any = {};
+  if (iscnIdPrefix) payload.iscnIdPrefix = iscnIdPrefix;
   if (inLanguage) payload.inLanguage = inLanguage;
   if (name) payload.name = name;
   if (description) payload.description = description;
