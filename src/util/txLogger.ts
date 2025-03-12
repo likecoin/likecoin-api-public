f91268bfff0d4f6d5b7bb809ec4e190f455b7d94
William Chong <me@williamchong.cloud>
2025-03-12 16:45:25 +0800

    🔥 Remove unused tx logger

--- src/util/txLogger.ts
+++ src/util/txLogger.ts
@@ -1,50 +1,5 @@
 import { txCollection as dbRef } from './firebase';
 
-export async function logTransferDelegatedTx(payload) {
-  const { txHash } = payload;
-  try {
-    await dbRef.doc(txHash).create({
-      type: 'transferDelegated',
-      status: 'pending',
-      ts: Date.now(),
-      ...payload,
-    });
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-}
-
-export async function logETHTx(payload) {
-  const { txHash } = payload;
-  try {
-    await dbRef.doc(txHash).create({
-      type: 'transferETH',
-      status: 'pending',
-      ts: Date.now(),
-      ...payload,
-    });
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-}
-
-export async function logClaimCouponTx(payload) {
-  const { txHash } = payload;
-  try {
-    await dbRef.doc(txHash).create({
-      type: 'claimCoupon',
-      status: 'pending',
-      ts: Date.now(),
-      ...payload,
-    });
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  }
-}
-
 export async function logCosmosTx(payload) {
   const { txHash } = payload;
   try {
