f73b2917c4dcce12156f1f9f65e380546d066d8b
William Chong <me@williamchong.cloud>
2025-03-18 04:28:12 +0800

    ♻️ Refactor mintNFTs to log signed tx to db

--- src/util/evm/client.ts
+++ src/util/evm/client.ts
@@ -1,4 +1,13 @@
-import { createPublicClient, createWalletClient, http } from 'viem';
+import {
+  Chain,
+  createPublicClient,
+  createWalletClient,
+  http,
+  HttpTransport,
+  LocalAccount,
+  PublicClient,
+  WalletClient,
+} from 'viem';
 import { optimism, optimismSepolia } from 'viem/chains';
 import { privateKeyToAccount } from 'viem/accounts';
 import { IS_TESTNET } from '../../constant';
@@ -6,25 +15,26 @@ import {
   LIKER_NFT_PRIVATE_KEY,
 } from '../../../config/secret';
 
-let client;
-let walletClient;
-export function getEvmClient() {
+let client: PublicClient<HttpTransport, Chain, undefined>;
+let walletClient: WalletClient<HttpTransport, Chain, LocalAccount>;
+
+export function getEvmClient(): PublicClient<HttpTransport, Chain, undefined> {
   if (!client) {
     client = createPublicClient({
       chain: IS_TESTNET ? optimismSepolia : optimism,
       transport: http(),
-    });
+    }) as PublicClient<HttpTransport, Chain, undefined>;
   }
   return client;
 }
 
-export function getEvmWalletAccount() {
+export function getEvmWalletAccount(): LocalAccount {
   const evmHex = LIKER_NFT_PRIVATE_KEY.toString('hex');
   const account = privateKeyToAccount(`0x${evmHex}`);
   return account;
 }
 
-export function getEvmWalletClient() {
+export function getEvmWalletClient(): WalletClient<HttpTransport, Chain, LocalAccount> {
   if (!walletClient) {
     const account = getEvmWalletAccount();
     walletClient = createWalletClient({
