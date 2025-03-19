96fc3e7140b4976569da63812bc34201187ed9f8
William Chong <me@williamchong.cloud>
2025-03-19 17:14:01 +0800

    ♻️ Split migrate all and migrate user endpoint

--- src/routes/wallet/index.ts
+++ src/routes/wallet/index.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 import { checkCosmosSignPayload, checkEvmSignPayload } from '../../util/api/users';
 import { ValidationError } from '../../util/ValidationError';
 import { jwtSign } from '../../util/jwt';
-import { findLikeWalletByEvmWallet, migrateLikeWalletToEvmWallet } from '../../util/api/wallet';
+import {
+  findLikeWalletByEvmWallet, migrateLikeUserToEvmUser, migrateLikeWalletToEvmWallet,
+} from '../../util/api/wallet';
 
 const router = Router();
 
@@ -51,7 +53,48 @@ router.post('/authorize', async (req, res, next) => {
   }
 });
 
-router.post('/evm/migrate', async (req, res, next) => {
+router.post('/evm/migrate/user', async (req, res, next) => {
+  try {
+    const {
+      cosmos_address: likeWallet,
+      cosmos_signature: signature,
+      cosmos_public_key: publicKey,
+      cosmos_signature_content: message, signMethod,
+    } = req.body;
+    if (!likeWallet || !signature || !publicKey || !message) throw new ValidationError('INVALID_PAYLOAD');
+    if (!publicKey) throw new ValidationError('INVALID_PAYLOAD');
+    const signed = checkCosmosSignPayload({
+      signature, publicKey, message, inputWallet: likeWallet, signMethod, action: 'migrate',
+    });
+    if (!signed) {
+      throw new ValidationError('INVALID_SIGN');
+    }
+    const { evm_wallet: evmWallet } = signed;
+    if (!evmWallet) {
+      throw new ValidationError('INVALID_PAYLOAD');
+    }
+    const {
+      isMigratedLikerId,
+      isMigratedLikerLand,
+      migratedLikerId,
+      migratedLikerLandUser,
+      migrateLikerIdError,
+      migrateLikerLandError,
+    } = await migrateLikeUserToEvmUser(likeWallet, evmWallet);
+    res.json({
+      isMigratedLikerId,
+      isMigratedLikerLand,
+      migratedLikerId,
+      migratedLikerLandUser,
+      migrateLikerIdError,
+      migrateLikerLandError,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/evm/migrate/all', async (req, res, next) => {
   try {
     const {
       cosmos_address: likeWallet,
