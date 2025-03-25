706cb0e96e4c53fb3040f40e29f4395cac96d34f
William Chong <me@williamchong.cloud>
2025-03-26 03:47:39 +0800

    ➕ Add viem

---
+++ src/util/evm/client.ts
@@ -0,0 +1,23 @@
+import {
+  Chain,
+  createPublicClient,
+  http,
+  HttpTransport,
+  PublicClient,
+} from 'viem';
+
+import { optimism, optimismSepolia } from 'viem/chains';
+import { IS_TESTNET } from '../../constant';
+
+let client: PublicClient<HttpTransport, Chain, undefined>;
+export function getEvmClient(): PublicClient<HttpTransport, Chain, undefined> {
+  if (!client) {
+    client = createPublicClient({
+      chain: IS_TESTNET ? optimismSepolia : optimism,
+      transport: http(),
+    }) as PublicClient<HttpTransport, Chain, undefined>;
+  }
+  return client;
+}
+
+export default getEvmClient;
