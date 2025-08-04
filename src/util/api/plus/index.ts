15df0fedff46b5873d2dc58cda2d89e6b2539268
William Chong <me@williamchong.cloud>
2025-08-05 03:01:51 +0800

    🔊 Add plus session metadata to payment_intent

--- src/util/api/plus/index.ts
+++ src/util/api/plus/index.ts
@@ -183,6 +183,9 @@ export async function createNewPlusCheckoutSession(
     metadata,
     mode: 'subscription',
     subscription_data: subscriptionData,
+    payment_intent_data: {
+      metadata,
+    },
     success_url: `https://${BOOK3_HOSTNAME}/plus/success?redirect=1&period=${period}&trial=${hasFreeTrial ? '1' : '0'}`,
     cancel_url: `https://${BOOK3_HOSTNAME}/plus`,
     payment_method_collection: mustCollectPaymentMethod ? 'always' : 'if_required',
