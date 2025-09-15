774956496071e86afa45296fa9f94cd702f80d7b
William Chong <me@williamchong.cloud>
2025-09-16 00:43:36 +0800

    🔥 Remove create ISCN API

--- src/util/cosmos/iscn.ts
+++ src/util/cosmos/iscn.ts
@@ -1,20 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { DirectSecp256k1Wallet } from '@cosmjs/proto-signing';
-import { ISCNQueryClient, ISCNSigningClient } from '@likecoin/iscn-js';
+import { ISCNQueryClient } from '@likecoin/iscn-js';
 import { getLikeWalletAddress } from '@likecoin/iscn-js/dist/iscn/addressParsing';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { AccountData } from '@cosmjs/amino';
-import { getAccountInfo } from '.';
 import { getUserWithCivicLikerProperties } from '../api/users/getPublicInfo';
-import { COSMOS_PRIVATE_KEY } from '../../../config/secret';
-import { COSMOS_RPC_ENDPOINT, COSMOS_SIGNING_RPC_ENDPOINT } from '../../../config/config';
+import { COSMOS_RPC_ENDPOINT } from '../../../config/config';
 
 export { parseTxInfoFromIndexedTx } from '@likecoin/iscn-js/dist/messages/parsing';
 
 let queryClient: ISCNQueryClient | null = null;
-let signingClient: ISCNSigningClient | null = null;
-let signingWallet: AccountData | null = null;
-let signingAccountNumber: number | null = null;
 
 export async function getISCNQueryClient() {
   if (!queryClient) {
@@ -23,37 +14,6 @@ export async function getISCNQueryClient() {
     queryClient = client;
   }
   return queryClient;
-}
-
-export async function createISCNSigningClient(privateKey) {
-  const privateKeyBytes = Buffer.from(privateKey, 'hex');
-  const signer = await DirectSecp256k1Wallet.fromKey(privateKeyBytes, 'like');
-  const [wallet] = await signer.getAccounts();
-  const client = new ISCNSigningClient();
-  await client.connectWithSigner(COSMOS_SIGNING_RPC_ENDPOINT, signer);
-  return { client, wallet };
-}
-
-export async function getISCNSigningClient() {
-  if (!signingClient) {
-    const { client, wallet } = await createISCNSigningClient(COSMOS_PRIVATE_KEY);
-    signingWallet = wallet;
-    signingClient = client;
-  }
-  return signingClient;
-}
-
-export async function getISCNSigningAddressInfo() {
-  if (!signingWallet) await getISCNSigningClient();
-  if (!signingWallet) throw new Error('CANNOT_FETCH_SIGNING_WALLET');
-  if (!signingAccountNumber) {
-    const { accountNumber } = await getAccountInfo(signingWallet.address);
-    signingAccountNumber = accountNumber.toNumber();
-  }
-  return {
-    address: signingWallet.address,
-    accountNumber: signingAccountNumber,
-  };
 }
 
 export function getISCNPrefix(input) {
