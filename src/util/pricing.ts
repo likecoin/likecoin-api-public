07d1234387918ce5b636cb27a04f24ac44e7b2cf
William Chong <me@williamchong.cloud>
2026-01-29 12:52:43 +0800

    🥅 Use max values for conversion rate if price is > pricing table

--- src/util/pricing.ts
+++ src/util/pricing.ts
@@ -1,15 +1,25 @@
 import { USD_PRICE_TIER_LIST, HKD_PRICE_TIER_LIST, TWD_PRICE_TIER_LIST } from '../constant/pricing';
 
+const MAX_USD = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1]!;
+const MAX_HKD = HKD_PRICE_TIER_LIST[HKD_PRICE_TIER_LIST.length - 1]!;
+const MAX_TWD = TWD_PRICE_TIER_LIST[TWD_PRICE_TIER_LIST.length - 1]!;
+
 export function convertUSDPriceToCurrency(price: number, currency: 'hkd' | 'twd' | 'usd'): number {
   if (price <= 0) {
     return 0;
   }
   switch (currency) {
     case 'hkd': {
+      if (price > MAX_USD) {
+        return Math.floor(price * (MAX_HKD / MAX_USD));
+      }
       const index = Math.min(Math.round(price), HKD_PRICE_TIER_LIST.length - 1);
       return HKD_PRICE_TIER_LIST[index];
     }
     case 'twd': {
+      if (price > MAX_USD) {
+        return Math.floor(price * (MAX_TWD / MAX_USD));
+      }
       const index = Math.min(Math.round(price), TWD_PRICE_TIER_LIST.length - 1);
       return TWD_PRICE_TIER_LIST[index];
     }
@@ -22,14 +32,18 @@ export function convertUSDPriceToCurrency(price: number, currency: 'hkd' | 'twd'
 export function convertCurrencyToUSDPrice(price: number, currency: 'hkd' | 'twd' | 'usd'): number {
   switch (currency) {
     case 'hkd': {
+      if (price > MAX_HKD) {
+        return Math.floor(price * (MAX_USD / MAX_HKD));
+      }
       const index = HKD_PRICE_TIER_LIST.findIndex((tierPrice) => tierPrice >= price);
-      const maxPrice = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1];
-      return index >= 0 ? USD_PRICE_TIER_LIST[index] : maxPrice;
+      return index >= 0 ? USD_PRICE_TIER_LIST[index] : MAX_USD;
     }
     case 'twd': {
+      if (price > MAX_TWD) {
+        return Math.floor(price * (MAX_USD / MAX_TWD));
+      }
       const index = TWD_PRICE_TIER_LIST.findIndex((tierPrice) => tierPrice >= price);
-      const maxPrice = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1];
-      return index >= 0 ? USD_PRICE_TIER_LIST[index] : maxPrice;
+      return index >= 0 ? USD_PRICE_TIER_LIST[index] : MAX_USD;
     }
     case 'usd':
     default:
