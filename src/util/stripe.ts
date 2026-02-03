9a1ab6958b27ab2f32f400c8d20fd36b929ae92a
William Chong <me@williamchong.cloud>
2026-02-03 17:09:19 +0800

    ⬆️ Upgrade stripe@20.3.0

--- src/util/stripe.ts
+++ src/util/stripe.ts
@@ -6,7 +6,7 @@ let stripeClient: Stripe | null = null;
 
 export function getStripeClient(): Stripe {
   if (!stripeClient) {
-    stripeClient = new Stripe(STRIPE_KEY, { apiVersion: '2025-12-15.clover', typescript: true });
+    stripeClient = new Stripe(STRIPE_KEY, { apiVersion: '2026-01-28.clover', typescript: true });
   }
   return stripeClient;
 }
